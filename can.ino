ee8fc4685dad8bb4c759f812e47b65bb2158e983
Charlie Smurthwaite <netmaster10000@yahoo.com>
2021-11-08 15:37:08 +0000

    add correct arduino file

---
+++ can.ino
@@ -0,0 +1,78 @@
+#include <CAN.h>
+
+void setup() {
+  Serial.begin(1000000);
+  CAN.begin(500000);
+  CAN.onReceive(onReceive);
+}
+
+// Call this a couple of times per second for continuous balancing
+void balance(float voltage) {
+  uint16_t v = voltage * 65535.0f / 5.0f;
+  CAN.beginPacket(0x4F8);
+  CAN.write(0x00); // Balance
+  CAN.write(v >> 8);
+  CAN.write(v);
+  CAN.endPacket();
+}
+
+void loop() {
+  delay(500);
+  // This program is current set up to balance to 3.5v all the time for testing.
+  // You probably want to set this to the voltage of the lowest cell and probably
+  // only above a certain minimum (eg your max charge).
+  balance(3.8);
+}
+
+// Calculate NTC temperature
+float temperature(uint16_t adc) {
+  float r = 0.0000000347363427499292f * adc * adc - 0.001025770762903f * adc + 2.68235340614337f;
+  float t = log(r) * -30.5280964239816f + 95.6841501312447f;
+  return t;
+}
+
+// Calculate ADC voltage
+float voltage(uint16_t adc) {
+  float v = adc * 5.0f / 65535.0f;
+  return v;
+}
+
+// Called when CAN data is received
+void onReceive(int packetSize) {
+  if(CAN.packetRtr()) return;         // Ignore rtr packets
+  if(CAN.packetId() != 0x4f0) return; // Ignore non-BMS packets
+
+  uint8_t module = CAN.read();
+  uint8_t cell = CAN.read();
+  uint16_t data = (uint16_t)CAN.read() << 8 | CAN.read();
+  if(module == 0xff) {
+  } else {
+    if(cell < 16) {
+      Serial.print("M:");
+      Serial.print(module);
+      Serial.print(" C:");
+      Serial.print(cell);
+      Serial.print(" ");
+      Serial.print(voltage(data), 3);
+      Serial.print("V\n");
+    } else if(cell == 17) {
+      Serial.print("M:");
+      Serial.print(module);
+      Serial.print(" NEG SIDE TEMP: ");
+      Serial.print(temperature(data), 2);
+      Serial.print("C\n");
+    } else if(cell == 18) {
+      Serial.print("M:");
+      Serial.print(module);
+      Serial.print(" POS SIDE TEMP: ");
+      Serial.print(temperature(data), 2);
+      Serial.print("C\n");
+    } else if(cell == 0xff) {
+      Serial.print("M:");
+      Serial.print(module);
+      Serial.print(" Balancing cells: ");
+      Serial.print(data, HEX);
+      Serial.print("\n");
+    }
+  }
+}
